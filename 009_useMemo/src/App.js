b71aca0ce4bca787dfad2a3c16f1629d0f3e9ceb
Anandhan <anandhanedom@gmail.com>
2021-01-05 00:38:17 +0530

    md

--- 009_useMemo/src/App.js
+++ 009_useMemo/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 //Components
+import Counter from './components/Counter';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
