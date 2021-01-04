9c590082ec855dc90c37e053b617f61ade456d7b
Anandhan <anandhanedom@gmail.com>
2021-01-05 00:52:48 +0530

    focus

--- 010_useRef/src/App.js
+++ 010_useRef/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 //Components
+import FocusInput from './components/FocusInput';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FocusInput />
+    </div>
+  );
 }
 
 export default App;
