c53f5ab619b98f38dfce2bb94632b3331d33844b
Anandhan <anandhanedom@gmail.com>
2021-01-05 17:01:26 +0530

    react

--- 011_customhooks/src/App.js
+++ 011_customhooks/src/App.js
@@ -5,16 +5,20 @@ import './App.css';
 // import DocTitleOne from './components/DocTitleOne';
 // import DocTitleTwo from './components/DocTitleTwo';
 
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
+// import CounterOne from './components/CounterOne';
+// import CounterTwo from './components/CounterTwo';
+
+import UserForm from './components/UserForm';
 
 function App() {
   return (
     <div className="App">
       {/* <DocTitleOne /> */}
       {/* <DocTitleTwo /> */}
-      <CounterOne />
-      <CounterTwo />
+      {/* <CounterOne />
+      <CounterTwo /> */}
+
+      <UserForm />
     </div>
   );
 }
