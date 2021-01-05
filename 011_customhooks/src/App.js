7c70eb15ff5c21cd302ae79f6927ec9c6908fd05
Anandhan <anandhanedom@gmail.com>
2021-01-05 15:39:51 +0530

    final

--- 011_customhooks/src/App.js
+++ 011_customhooks/src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './App.css';
 
 //Components
+// import DocTitleOne from './components/DocTitleOne';
+// import DocTitleTwo from './components/DocTitleTwo';
+
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <DocTitleOne /> */}
+      {/* <DocTitleTwo /> */}
+      <CounterOne />
+      <CounterTwo />
+    </div>
+  );
 }
 
 export default App;
