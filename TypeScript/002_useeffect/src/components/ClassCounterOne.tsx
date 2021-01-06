9cd60b33cc4ab0a691bea34bb170df1548948c3c
Anandhan <anandhanedom@gmail.com>
2021-01-06 16:53:38 +0530

    use effecting

---
+++ TypeScript/002_useeffect/src/components/ClassCounterOne.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+interface ICounter {
+  count: number;
+  name: string;
+}
+
+class ClassCounterOne extends Component<any, ICounter> {
+  // optional second annotation for better type inference
+
+  state: ICounter = {
+    count: 0,
+    name: '',
+  };
+
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`;
+  }
+
+  componentDidUpdate(prevProps: any, prevState: ICounter) {
+    if (prevState.count !== this.state.count) {
+      console.log('Updating document title');
+      document.title = `Clicked ${this.state.count} times`;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <button
+          onClick={() =>
+            this.setState((prevState) => ({ count: prevState.count + 1 }))
+          }
+        >
+          Click {this.state.count} times
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ClassCounterOne;
