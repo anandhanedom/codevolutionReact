90717e9e5ddae38b585312c3bc1eb500fc9fc2e9
Anandhan <anandhanedom@gmail.com>
2021-01-19 11:12:53 +0530

    add eslint prettier ts config

--- Projects/TS/0001_uxlaws/src/App.tsx
+++ Projects/TS/0001_uxlaws/src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
-//Pages
+// Pages
 import HomePage from './pages/home/home.pages';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <HomePage />
