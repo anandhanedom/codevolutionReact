f1643c4a68be37470caa2e81d0371b450f9581c4
Anandhan <anandhanedom@gmail.com>
2021-01-28 23:29:17 +0530

    suspensing

--- concurrentMode/01_suspense/src/Api.js
+++ concurrentMode/01_suspense/src/Api.js
@@ -18,9 +18,34 @@ const fetchPosts = () => {
     .catch((err) => console.log(err));
 };
 
-const wrapPromise = () => {
+const wrapPromise = (promise) => {
   // Set the initial status
   let status = 'pending';
+
+  // Store results
+  let result;
+
+  let suspender = promise
+    .then((res) => {
+      status = 'success';
+      result = res;
+    })
+    .catch((err) => {
+      status = 'error';
+      result = err;
+    });
+
+  return {
+    read() {
+      if (status === 'pending') {
+        throw suspender;
+      } else if (status === 'error') {
+        throw result;
+      } else if (status === 'success') {
+        return result;
+      }
+    },
+  };
 };
 
 export const fetchData = () => {
