fa82c0ba88caa342be3dc61d6716af945ea13229
Anandhan <anandhanedom@gmail.com>
2021-02-01 20:23:28 +0530

    generators

--- iteratorsAndGens/generators/app.js
+++ iteratorsAndGens/generators/app.js
@@ -1,5 +1,25 @@
 // Generators
 
+function* generator() {
+  yield 1;
+  yield* anotherGen();
+
+  return 'hello'; // no more yielding from here
+
+  yield 3;
+}
+
+function* anotherGen() {
+  yield 2;
+}
+
+let geGen = generator();
+
+console.log(geGen.next());
+console.log(geGen.next());
+console.log(geGen.next());
+console.log(geGen.next());
+
 // function* sayNames(params) {
 //   yield 'Jack';
 //   yield 'Jill';
@@ -14,18 +34,18 @@
 // console.log(name.next().value);
 
 // ID creator
-function* createIds(params) {
-  let index = 1;
+// function* createIds(params) {
+//   let index = 1;
 
-  while (true) {
-    yield index++;
-  }
-}
+//   while (true) {
+//     yield index++;
+//   }
+// }
 
-const gen = createIds();
+// const gen = createIds();
 
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
