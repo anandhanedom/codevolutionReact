83b179fc32e9276497729f610576ee37e3ec80a2
Anandhan <anandhanedom@gmail.com>
2021-02-05 10:36:57 +0530

    usecallback and usememo

---
+++ TechSith/01_hooks/004_useCallback/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//Components
+import ParentComponent from './components/ParentComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
